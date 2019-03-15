b3785b1d51f8fa6caf8e6f6b26377d261b302139
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2019-03-15 04:38:47 +0000

    Use string rather than using fmod

--- PowDigitSum.c
+++ PowDigitSum.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
+//#include <math.h>
+
+#define MAX_STR_LEN 500
 
 double myPow(int base, int exp);
 
@@ -8,15 +10,27 @@ main()
 {
     int res = 0;
     double x = myPow(2, 1000);
+    char str_x[MAX_STR_LEN];    // since using fmod has a problem, 
+                                // use string to calculate the sum of its digits
     
-    while(x > 0)
+    // while(x > 0)
+    // {
+    //     res += fmod(x, 10);
+    //     x /= 10;
+    // }
+    
+    snprintf(str_x, MAX_STR_LEN, "%.0lf", x);
+    char *str_x_ptr = str_x;
+    
+    while(*str_x_ptr)
     {
-        res += fmod(x, 10);
-        x /= 10;
+        res += *str_x_ptr - '0';
+        str_x_ptr++;
     }
     
     printf("%d\n", res);
 
+    
 }
 
 double myPow(int base, int exp)
